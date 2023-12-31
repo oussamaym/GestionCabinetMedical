a7c5d7254738ea936b3b052a4dee7dbfc758d28b
oussamaym <oussamaelysm15@gmail.com>
2023-12-31 22:14:57 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestionCabinetMedical.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
