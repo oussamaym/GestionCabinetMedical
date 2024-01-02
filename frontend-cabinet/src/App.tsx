c0e3ea23d0fa8b1074576cccc8445a2f627ec24f
Elkarni Bachar <diamobachar06@gmail.com>
2024-01-02 01:47:28 +0100

    started the front-end template

--- frontend-cabinet/src/App.tsx
+++ frontend-cabinet/src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Index from "./components";
+import "./css/animate.min.css";
+import "./css/bootstrap.min.css";
+import "./css/datepicker.css";
+import "./css/font-awesome.min.css";
+import "./css/icofont.css";
+import "./css/magnific-popup.css";
+import "./css/nice-select.css";
+import "./css/normalize.css"
+import "./css/owl-carousel.css"
+import "./css/responsive.css"
+import "./css/slicknav.min.css"
+import "./css/style.css"
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Index/>
+    </div>
+  );
+};
 
-export default App
+export default App;
